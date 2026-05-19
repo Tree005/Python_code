3a219eae2a6a62b06d146c7f83a88cf29957781f
Tree <2958846673@qq.com>
2026-05-19 16:39:40 +0800

    打包解包

--- Hello/08_进阶/02_多返回值_参数的打包与解包.py
+++ Hello/08_进阶/02_多返回值_参数的打包与解包.py
@@ -1,48 +1,23 @@
-# 一、函数的多返回值
-# def calculate(x, y):
-#     res1 = x + y
-#     res2 = x - y
-#     return res1, res2
-#
-# result = calculate(30, 10)
-# print(result)
-#
-# r1, r2 = calculate(30, 10)
-# print(r1, r2)
+# 多返回值 返回元组类型
+def  calculate(x,y):
+    res1 = x + y
+    res2 = x - y
+    return res1,res2
+result = calculate(30,10)
+print(result)
+print(type(result))
+#元组拆包
+r1,r2 = calculate(30,10)
+print(r1)
+print(r2)
+#*args 把所有传进来的位置参数打包成一个元组
+# **kwargs — 打包关键字参数
+def show_info(*args,**kwargs):
+    print('位置参数：',args)
+    print('关键字参数：',kwargs)
+show_info(10,20,30,name = '张三',age = 18)
 
-
-# 二、参数的打包与解包
-
-# 1.打包接收参数：
-# *args    ：打包所有的位置参数（会形成一个元组）
-# **kwargs ：打包所有的关键字参数（会形成一个字典）
-# def show_info(*args, **kwargs):
-#     print(args)
-#     print(kwargs)
-#
-# show_info(10, 20, 30, name='张三', age=18, gender='男')
-
-
-# 2.解包传递参数
-# *变量名  ：将元组拆解成一个一个独立的位置参数
-# **变量名 ：将字典拆解一个一个 key=value 形式的关键字参数
-# def show_info(num1, num2, num3, name, age, gender):
-#     print(num1, num2, num3)
-#     print(name, age, gender)
-#
-# nums = (10, 20, 30)
-# person = {'name':'张三', 'age':18, 'gender':'男'}
-#
-# show_info(*nums, **person)
-
-
-# 3.打包接收参数 和 解包传递参数 一起使用
-# def show_info(*args, **kwargs):
-#     print(args)
-#     print(kwargs)
-#
-# nums = (10, 20, 30)
-# person = {'name':'张三', 'age':18, 'gender':'男'}
-#
-# show_info(*nums, **person)
-
+#解包传递参数
+nums = (10, 20, 30)
+person = {'name': '张三', 'age': 18, 'gender': '男'}
+show_info(*nums,**person)
